0a0759af2e300b6f7b2f21c35e61a6bbbba73790
youssef <youssef.mastoura@esprit.tn>
2025-09-22 22:39:38 +0100

    ...

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,6 +8,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
+                echo 'Checking out code from GitHub...'
                 git branch: 'main', url: 'https://github.com/Youssef-Mastoura/jenkinstest.git'
             }
         }
@@ -15,19 +16,20 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building the Spring Boot project...'
-                sh "${MVN_HOME}/mvn clean compile"
+                sh "${MVN_HOME}/mvn clean compile -DskipTests -Dmaven.test.skip=true"
             }
         }
 
         stage('Package') {
             steps {
                 echo 'Packaging the application...'
-                sh "${MVN_HOME}/mvn package"
+                sh "${MVN_HOME}/mvn package -DskipTests -Dmaven.test.skip=true"
             }
         }
 
         stage('Archive Artifact') {
             steps {
+                echo 'Archiving the JAR artifact...'
                 archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
             }
         }
