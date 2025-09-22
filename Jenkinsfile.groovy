4cb77b52c9cf9ce0a270abd807c1414012f1bbf5
youssef <youssef.mastoura@esprit.tn>
2025-09-22 22:32:35 +0100

    ..

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,13 +2,12 @@ pipeline {
     agent any  // Run on any available Jenkins agent
 
     environment {
-        MVN_HOME = "/usr/bin"  // Updated Maven path
+        MVN_HOME = "/usr/bin" // Updated Maven path
     }
 
     stages {
         stage('Checkout') {
             steps {
-                echo 'Checking out code from GitHub...'
                 git branch: 'main', url: 'https://github.com/Youssef-Mastoura/jenkinstest.git'
             }
         }
@@ -17,14 +16,6 @@ pipeline {
             steps {
                 echo 'Building the Spring Boot project...'
                 sh "${MVN_HOME}/mvn clean compile"
-            }
-        }
-
-        stage('Test') {
-            steps {
-                echo 'Running unit tests against MySQL...'
-                // Ensure MySQL is running and accessible
-                sh "${MVN_HOME}/mvn test"
             }
         }
 
@@ -37,7 +28,6 @@ pipeline {
 
         stage('Archive Artifact') {
             steps {
-                echo 'Archiving the JAR artifact...'
                 archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
             }
         }
